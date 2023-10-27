4a45df043e3b56baa5f2091774098829e30731fe
hcssmith <66956565+hcssmith@users.noreply.github.com>
2023-10-27 20:53:36 +0100

    Add Neogit (#1)
    * Install neogit
    
    * New keybind to launch Neogit
    
    * Typo in keymap

--- init.lua
+++ init.lua
@@ -8,6 +8,7 @@ local mtl = {
   'modules.opt.dracula',
   'modules.opt.gitsigns',
   'modules.opt.gitworktree',
+  'modules.opt.neogit',
   'modules.opt.nvimtree',
   'modules.opt.vimtex',
   'modules.opt.ufo',
