5c15599e5e4ffe3cd14ebf9d62fe1108de8c6ed4
hcssmith <66956565+hcssmith@users.noreply.github.com>
2023-10-27 19:50:51 +0100

    Install neogit

---
+++ lua/modules/opt/neogit.lua
@@ -0,0 +1,18 @@
+
+M = {}
+
+M.lazy = {
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",         -- required
+    "nvim-telescope/telescope.nvim", -- optional
+    "sindrets/diffview.nvim",        -- optional
+    "ibhagwan/fzf-lua",              -- optional
+  },
+  config = function ()
+    require("neogit").setup({
+
+    })
+  end
+}
+return M
