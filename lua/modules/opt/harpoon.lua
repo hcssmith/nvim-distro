cf48c4905eba6cbe271e5d8bb854c87f1a14d309
hcssmith <66956565+hcssmith@users.noreply.github.com>
2023-10-28 14:40:52 +0100

    More files in keymap

--- lua/modules/opt/harpoon.lua
+++ lua/modules/opt/harpoon.lua
@@ -7,7 +7,13 @@ M.keymap = {
     {'<leader>hm', function () require("harpoon.mark").add_file() end},
     {'<leader>h1', function () require("harpoon.ui").nav_file(1) end},
     {'<leader>h2', function () require("harpoon.ui").nav_file(2) end},
-    {'<leader>h3', function () require("harpoon.ui").nav_file(3) end}
+    {'<leader>h3', function () require("harpoon.ui").nav_file(3) end},
+    {'<leader>h4', function () require("harpoon.ui").nav_file(4) end},
+    {'<leader>h5', function () require("harpoon.ui").nav_file(5) end},
+    {'<leader>h6', function () require("harpoon.ui").nav_file(6) end},
+    {'<leader>h7', function () require("harpoon.ui").nav_file(7) end},
+    {'<leader>h8', function () require("harpoon.ui").nav_file(8) end},
+    {'<leader>h9', function () require("harpoon.ui").nav_file(9) end}
   }
 }
 
