e57849677c778f26bf945b96ae226e0f30431b79
Hallam Smith <me@hcssmith.com>
2023-11-05 16:44:21 +0000

    Swap to API function

--- lua/modules/opt/nvimtree.lua
+++ lua/modules/opt/nvimtree.lua
@@ -11,7 +11,8 @@ M.keymap = {
 }
 
 function M.settings()
-  vim.highlight.create('NvimTreeNormal', {guibg='none'})
+  --vim.highlight.create('NvimTreeNormal', {guibg='none'})
+  vim.api.nvim_set_hl(0, 'NvimTreeNormal', {guibg='none'})
 end
 
 M.lazy = {
