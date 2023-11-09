63fd7fa725fbcb4990c601d280fece8301f1faff
Hallam Smith <me@hcssmith.com>
2023-11-09 23:02:47 +0000

    Fix dashboard

--- lua/core/util/functions.lua
+++ lua/core/util/functions.lua
@@ -12,4 +12,16 @@ function M.Footer()
   end
 end
 
+function LspActive()
+  local lsp_clients = vim.lsp.get_clients({bufnr=0})
+  if #lsp_clients > 0 then
+    return true
+  else
+    return false
+  end
+end
+
+function LspInactive()
+  return not LspActive()
+end
 return M
--- lua/modules/base/dashboard.lua
+++ lua/modules/base/dashboard.lua
@@ -108,7 +108,17 @@ M.lazy = {
           action = ':q'
         },
       },
-      footer = {}
+      footer = function ()
+        local version = " " .. vim.version().major .. "." .. vim.version().minor .. "." .. vim.version().patch
+        local lazy_ok, lazy = pcall(require, "lazy")
+        if lazy_ok then
+          local total_plugins = lazy.stats().count .. " Plugins"
+          local startuptime = (math.floor(lazy.stats().startuptime * 100 + 0.5) / 100)
+          return { version .. "   " .. total_plugins .. "  󰄉 " .. startuptime .. " ms" }
+        else
+          return { version }
+        end
+      end
     }
   },
   dependencies = { {'nvim-tree/nvim-web-devicons'}}
