084b22f594cab15155c2adacb7f7b7154439046e
Hallam Smith <me@hcssmith.com>
2023-11-10 17:26:43 +0000

    Fully setup lazy loading for nvim-tree

--- lua/modules/nvimtree.lua
+++ lua/modules/nvimtree.lua
@@ -19,6 +19,20 @@ return {
         api.tree.toggle()
       end, desc = 'Toggle nvim-tree'}
     },
+    init = function ()
+      vim.g.loaded_netrw = 1
+      vim.g.loaded_netrwPlugin = 1
+      local augroup = vim.api.nvim_create_augroup('nvim_tree_cmds', {clear = true})
+      vim.api.nvim_create_autocmd('BufEnter', {
+        group = augroup,
+        desc = 'Open NvimTree on Dir',
+        callback = function(event)
+          if vim.fn.isdirectory(vim.api.nvim_buf_get_name(event.buf)) ~= 0 then
+            require('nvim-tree.api').tree.open()
+          end
+        end
+      })
+    end,
     config = function (_, opts)
       require("nvim-tree").setup(opts)
       vim.api.nvim_set_hl(0, 'NvimTreeNormal', {bg='none'})
