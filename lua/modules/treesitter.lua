3f832d33e48c29de5e1c1f6243d3850743706dbd
hcssmith <me@hcssmith.com>
2023-11-11 12:41:58 +0000

    Treesitter configuration (#34)

--- lua/modules/treesitter.lua
+++ lua/modules/treesitter.lua
@@ -1,63 +1,73 @@
-return {
-    'nvim-treesitter/nvim-treesitter',
-    cond = HasCCompiler,
-    build = ":TSUpdate",
-    event = 'BufRead',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects'
+return
+{
+  'nvim-treesitter/nvim-treesitter',
+  cond = HasCCompiler,
+  build = ":TSUpdate",
+  event = 'BufRead',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+  },
+  main = 'nvim-treesitter.configs',
+  keys = {
+    {'<leader>ts', function () vim.treesitter.inspect_tree() end},
+    {'<leader>tq', function () vim.treesitter.query.edit() end}
+  },
+  opts = {
+    ensure_installed = { "vimdoc", "javascript", "typescript", "c", "lua", "rust", "odin", "markdown", "markdown_inline", "query" },
+    modules = {},
+    ignore_install = {},
+    sync_install = false,
+    auto_install = true,
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+      disable = {
+        "make"
+      },
     },
-    config = function () require'nvim-treesitter.configs'.setup({
-      ensure_installed = { "vimdoc", "javascript", "typescript", "c", "lua", "rust", "odin", "markdown", "markdown_inline" },
-      modules = {},
-      ignore_install = {},
-      sync_install = false,
-      auto_install = true,
-      highlight = {
+    playground = {
+      enable = true,
+      disable = {},
+      updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+      persist_queries = false, -- Whether the query persists across vim sessions
+      keybindings = {
+        toggle_query_editor = 'o',
+        toggle_hl_groups = 'i',
+        toggle_injected_languages = 't',
+        toggle_anonymous_nodes = 'a',
+        toggle_language_display = 'I',
+        focus_language = 'f',
+        unfocus_language = 'F',
+        update = 'R',
+        goto_node = '<cr>',
+        show_help = '?',
+      },
+    },
+    textobjects = {
+      lsp_interop = {
         enable = true,
-        additional_vim_regex_highlighting = false,
-        disable = {
-          "make"
+        border = 'none',
+        floating_preview_opts = {},
+        peek_definition_code = {
+          ["<leader>df"] = "@function.outer",
+          ["<leader>dF"] = "@class.outer",
         },
       },
-      textobjects = {
-        select = {
-          enable = true,
-          -- Automatically jump forward to textobj, similar to targets.vim
-          lookahead = true,
-          keymaps = {
-            -- You can use the capture groups defined in textobjects.scm
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            -- You can optionally set descriptions to the mappings (used in the desc parameter of
-            -- nvim_buf_set_keymap) which plugins like which-key display
-            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-            -- You can also use captures from other query groups like `locals.scm`
-            ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-          },
-          -- You can choose the select mode (default is charwise 'v')
-          --
-          -- Can also be a function which gets passed a table with the keys
-          -- * query_string: eg '@function.inner'
-          -- * method: eg 'v' or 'o'
-          -- and should return the mode ('v', 'V', or '<c-v>') or a table
-          -- mapping query_strings to modes.
-          selection_modes = {
-            ['@parameter.outer'] = 'v', -- charwise
-            ['@function.outer'] = 'V', -- linewise
-            ['@class.outer'] = '<c-v>', -- blockwise
-          },
-          -- If you set this to `true` (default is `false`) then any textobject is
-          -- extended to include preceding or succeeding whitespace. Succeeding
-          -- whitespace has priority in order to act similarly to eg the built-in
-          -- `ap`.
-          --
-          -- Can also be a function which gets passed a table with the keys
-          -- * query_string: eg '@function.inner'
-          -- * selection_mode: eg 'v'
-          -- and should return true of false
-          include_surrounding_whitespace = true,
+      select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["ip"] = "@parameter.inner",
         },
-      }
-    }) end
+        selection_modes = {
+          ['@parameter.inner'] = 'v', -- charwise
+          ['@function.outer'] = 'V', -- linewise
+          ['@function.inner'] = 'V', -- linewise
+        },
+        include_surrounding_whitespace = true,
+      },
+    }
   }
+}
