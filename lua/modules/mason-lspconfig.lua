66c99b86d2f32a5bbf9f09d8ae263d8d343578c7
Hallam Smith <me@hcssmith.com>
2023-11-19 19:34:06 +0000

    Setup lua_ls properly

--- lua/modules/mason-lspconfig.lua
+++ lua/modules/mason-lspconfig.lua
@@ -1,11 +1,15 @@
 return {
   'williamboman/mason-lspconfig.nvim',
-  opts = function ()
+  opts = function()
     local lsp_zero = require('lsp-zero')
     return {
       handlers = {
-        lsp_zero.default_setup
+        lsp_zero.default_setup,
+        lua_ls = function()
+          local lua_opts = lsp_zero.nvim_lua_ls()
+          require('lspconfig').lua_ls.setup(lua_opts)
+        end,
       }
     }
-    end
+  end
 }
