7ee394294208879e3d5deff6622cf40cff1512da
Hallam Smith <me@hcssmith.com>
2023-11-19 21:20:59 +0000

    Added NotWindows cond

--- lua/modules/composer.lua
+++ lua/modules/composer.lua
@@ -1,6 +1,7 @@
 return  {
   "ecthelionvi/NeoComposer.nvim",
   dependencies = { "kkharji/sqlite.lua" },
+  cond = NotWindows,
   lazy = true,
   keys = {
       {"<leader>m", function() require('NeoComposer.telescope.macros').show_macros() end}
