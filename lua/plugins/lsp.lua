b95538b7e2a2e3bfa1df72e88167563480382276
hcssmith <me@hcssmith.com>
2023-12-22 21:23:29 +0000

    Add explicit diagnostic enable. (#66)

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,19 +3,20 @@ return {
   'VonHeikemen/lsp-zero.nvim',
   branch = 'v3.x',
   lazy = true,
-  config = function (_, _)
+  config = function(_, _)
     local lsp_group = vim.api.nvim_create_augroup('LspUserAutoCmd', {})
     vim.api.nvim_create_autocmd('BufWritePre', {
       desc = 'Auto run lsp format on save if a lsp client with the correct capabilitiy is connected',
       group = lsp_group,
       ---@param ev AutoCmdEvent
-      callback = function (ev)
+      callback = function(ev)
         local clients = vim.lsp.get_clients({
           bufnr = ev.buf
         })
         for _, client in pairs(clients) do
           if client.attached_buffers[ev.buf] == true and client.server_capabilities.documentFormattingProvider then
             vim.lsp.buf.format()
+            vim.diagnostic.enable(ev.buf)
             return
           end
         end
