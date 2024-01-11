e4130874a766cd97e32249ba2136916bd4823e65
Hallam Smith <me@hcssmith.com>
2024-01-11 00:23:11 +0000

    Setup auto folding on first enrty for neorg files, with custom ufo bar

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -2,15 +2,55 @@
 return {
   'kevinhwang91/nvim-ufo',
   --cond = NotWindows,
+  event = { 'BufEnter' },
   dependencies = {
-    'kevinhwang91/promise-async'
+    'kevinhwang91/promise-async',
+    'nvim-treesitter/nvim-treesitter'
   },
-  event = 'LspAttach',
   keys = {
     { 'zR', function() require('ufo').openAllFolds() end },
     { 'zM', function() require('ufo').closeAllFolds() end }
   },
-  opts = {},
+  opts = function(_, _)
+    return {
+      provider_selector = function(bufnr, filetype, buftype)
+        return { 'treesitter', 'indent' }
+      end,
+      fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
+        local newVirtText = {}
+        local suffix = (' 󰁂 %d '):format(endLnum - lnum)
+        local sufWidth = vim.fn.strdisplaywidth(suffix)
+        local targetWidth = width - sufWidth
+        local curWidth = 0
+        for _, chunk in ipairs(virtText) do
+          local chunkText = chunk[1]
+          local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+          curWidth = chunkWidth + curWidth
+          if targetWidth > curWidth + chunkWidth then
+            chunk[2] = '@comment'
+            table.insert(newVirtText, chunk)
+          else
+            chunkText = truncate(chunkText, targetWidth - curWidth)
+            local hlGroup = chunk[2]
+            table.insert(newVirtText, { chunkText, hlGroup })
+            chunkWidth = vim.fn.strdisplaywidth(chunkText)
+            if curWidth + chunkWidth < targetWidth then
+              suffix = suffix .. ('·'):rep(targetWidth - curWidth - chunkWidth)
+            end
+            break
+          end
+        end
+        table.insert(newVirtText, { suffix, '@comment' })
+        local spacing = (width - sufWidth - curWidth)
+        local pad = ('·'):rep(spacing)
+        table.insert(newVirtText, {
+          pad,
+          '@comment'
+        })
+        return newVirtText
+      end
+    }
+  end,
   init = function(_)
     vim.o.foldcolumn = '1' -- '0' is not bad
     vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
